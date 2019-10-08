499f51251aab188c3e0501b6256145dd416693ce
fahri2404200 <49123071+fahri2404200@users.noreply.github.com>
2019-10-08 16:11:05 +0800

    Create try-except-finally.py

---
+++ try-except-finally.py
@@ -0,0 +1,14 @@
+def main ():
+   try:
+   
+      f = open("data.txt", "w")
+      
+      try:
+          f.write("Pemrograman Python")
+      finally:
+         f.close()
+    except IoError:
+       print("\nERROR: File tidak dapat " + " dibuka/ditulis")
+       
+if __name__ == "__main__":
+   main()
